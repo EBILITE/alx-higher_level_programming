329841aa7b9a53d66d391efc01b72063b6902678
EBILITE <ebilitenelson@gmail.com>
2022-07-11 10:41:03 +0100

    Added #!/usr/bin/python3 to 0-safe-print-list

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -1,3 +1,4 @@
+#!/usr/bin/python3
 def safe_print_list(my_list=[], x=0):
     printed = 0
     try:
@@ -8,4 +9,3 @@ def safe_print_list(my_list=[], x=0):
         pass
     print()
     return printed
-    
